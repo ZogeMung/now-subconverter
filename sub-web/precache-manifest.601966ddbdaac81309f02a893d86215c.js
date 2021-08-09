1c08298c5d0ad817145adedc3947c1a1ebdcce13
GitHub Action <noreply@github.com>
2021-08-09 05:04:54 +0000

    Update sub-web from ZogeMung/sub-web

---
+++ sub-web/precache-manifest.601966ddbdaac81309f02a893d86215c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "5abd119597254bebb346",
+    "url": "/css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "8767e6a109ad7b9fbebb",
+    "url": "/css/chunk-vendors.1019b600.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "aba94f7bb9f2384ddad43f862c8e1e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5abd119597254bebb346",
+    "url": "/js/app.9ba26596.js"
+  },
+  {
+    "revision": "a7f56551741fee1382a0",
+    "url": "/js/chunk-258412ab.591e442d.js"
+  },
+  {
+    "revision": "8767e6a109ad7b9fbebb",
+    "url": "/js/chunk-vendors.147e3762.js"
+  },
+  {
+    "revision": "417c67dfde0c71bc33e953d4e7d635f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
